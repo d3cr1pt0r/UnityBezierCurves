882de7911087c94f7700cd7c0c92e044cb295577
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-03-25 18:36:07 +0100

    * added curve index (needed to place new points on the curve)

--- Assets/Scripts/CurvePoint.cs
+++ Assets/Scripts/CurvePoint.cs
@@ -7,11 +7,15 @@ public class CurvePoint
 	public Vector3 normal;
 	public Vector3 tangent;
 
-	public CurvePoint (Vector3 position, Vector3 normal, Vector3 tangent)
+	public int curveIndex;
+
+	public CurvePoint (Vector3 position, Vector3 normal, Vector3 tangent, int curveIndex = 0)
 	{
 		this.position = position;
 		this.normal = normal;
 		this.tangent = tangent;
+
+		this.curveIndex = curveIndex;
 	}
 
 }
